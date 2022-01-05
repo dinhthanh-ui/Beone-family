46464552d4380c2caf2cb171922647e85a9e5731
Macbook Pro <macbookpro@MacBook-Pro-cua-Macbook.local>
2022-01-05 22:55:02 +0700

    chua xong

--- js/main.js
+++ js/main.js
@@ -9,12 +9,12 @@
 
 'use strict';
 
-(function ($) {
+(function($) {
 
     /*------------------
         Preloader
     --------------------*/
-    $(window).on('load', function () {
+    $(window).on('load', function() {
         $(".loader").fadeOut();
         $("#preloder").delay(200).fadeOut("slow");
     });
@@ -22,8 +22,8 @@
     /*------------------
         Background Set
     --------------------*/
-    $('.set-bg').each(function () {
-        var bg = $(this).data('setbg');
+    $('.set-bg').each(function() {
+        let bg = $(this).data('setbg');
         $(this).css('background-image', 'url(' + bg + ')');
     });
 
@@ -55,7 +55,7 @@
     /*------------------
         Product Slider
     --------------------*/
-   $(".product-slider").owlCarousel({
+    $(".product-slider").owlCarousel({
         loop: true,
         margin: 25,
         nav: true,
@@ -119,91 +119,93 @@
         autoHeight: false,
         autoplay: true,
     });
-    
+
     /*------------------
         CountDown
     --------------------*/
     // For demo preview
-    var today = new Date();
-    var dd = String(today.getDate()).padStart(2, '0');
-    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-    var yyyy = today.getFullYear();
+    let today = new Date();
+    let dd = String(today.getDate()).padStart(2, '0');
+    let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    let yyyy = today.getFullYear();
 
-    if(mm == 12) {
+    if (mm == 12) {
         mm = '01';
         yyyy = yyyy + 1;
     } else {
         mm = parseInt(mm) + 1;
         mm = String(mm).padStart(2, '0');
     }
-    var timerdate = mm + '/' + dd + '/' + yyyy;
+    let timerdate = mm + '/' + dd + '/' + yyyy;
     // For demo preview end
 
-    console.log(timerdate);
-    
 
     // Use this for real timer date
-    /* var timerdate = "2020/01/01"; */
+    /* let timerdate = "2020/01/01"; */
 
-	$("#countdown").countdown(timerdate, function(event) {
-        $(this).html(event.strftime("<div class='cd-item'><span>%D</span> <p>Days</p> </div>" + "<div class='cd-item'><span>%H</span> <p>Hrs</p> </div>" + "<div class='cd-item'><span>%M</span> <p>Mins</p> </div>" + "<div class='cd-item'><span>%S</span> <p>Secs</p> </div>"));
+    $("#countdown").countdown(timerdate, function(event) {
+        $(this).html(event.strftime("<div class='cd-item'><span>%H</span> <p>Hrs</p> </div>" + "<div class='cd-item'><span>%M</span> <p>Mins</p> </div>" + "<div class='cd-item'><span>%S</span> <p>Secs</p> </div>"));
     });
 
-        
+
     /*----------------------------------------------------
      Language Flag js 
     ----------------------------------------------------*/
     $(document).ready(function(e) {
-    //no use
-    try {
-        var pages = $("#pages").msDropdown({on:{change:function(data, ui) {
-            var val = data.value;
-            if(val!="")
-                window.location = val;
-        }}}).data("dd");
+        //no use
+        try {
+            let pages = $("#pages").msDropdown({
+                on: {
+                    change: function(data, ui) {
+                        let val = data.value;
+                        if (val != "")
+                            window.location = val;
+                    }
+                }
+            }).data("dd");
 
-        var pagename = document.location.pathname.toString();
-        pagename = pagename.split("/");
-        pages.setIndexByValue(pagename[pagename.length-1]);
+            let pagename = document.location.pathname.toString();
+            pagename = pagename.split("/");
+            pages.setIndexByValue(pagename[pagename.length - 1]);
+            $("#ver").html(msBeautify.version.msDropdown);
+        } catch (e) {
+            // console.log(e);
+        }
         $("#ver").html(msBeautify.version.msDropdown);
-    } catch(e) {
-        // console.log(e);
-    }
-    $("#ver").html(msBeautify.version.msDropdown);
 
-    //convert
-    $(".language_drop").msDropdown({roundedBorder:false});
+        //convert
+        $(".language_drop").msDropdown({ roundedBorder: false });
         $("#tech").data("dd");
     });
     /*-------------------
 		Range Slider
 	--------------------- */
-	var rangeSlider = $(".price-range"),
-		minamount = $("#minamount"),
-		maxamount = $("#maxamount"),
-		minPrice = rangeSlider.data('min'),
-		maxPrice = rangeSlider.data('max');
-	    rangeSlider.slider({
-		range: true,
-		min: minPrice,
+    let rangeSlider = $(".price-range"),
+        minamount = $("#minamount"),
+        maxamount = $("#maxamount"),
+        minPrice = rangeSlider.data('min'),
+        maxPrice = rangeSlider.data('max');
+    rangeSlider.slider({
+        range: true,
+        min: minPrice,
         max: maxPrice,
-		values: [minPrice, maxPrice],
-		slide: function (event, ui) {
-			minamount.val('$' + ui.values[0]);
-			maxamount.val('$' + ui.values[1]);
-		}
-	});
-	minamount.val('$' + rangeSlider.slider("values", 0));
+        values: [minPrice, maxPrice],
+        slide: function(event, ui) {
+            minamount.val('$' + ui.values[0]);
+            maxamount.val('$' + ui.values[1]);
+        }
+    });
+    minamount.val('$' + rangeSlider.slider("values", 0));
     maxamount.val('$' + rangeSlider.slider("values", 1));
 
     /*-------------------
 		Radio Btn
 	--------------------- */
-    $(".fw-size-choose .sc-item label, .pd-size-choose .sc-item label").on('click', function () {
+    $(".fw-size-choose .sc-item label, .pd-size-choose .sc-item label").on('click', function() {
         $(".fw-size-choose .sc-item label, .pd-size-choose .sc-item label").removeClass('active');
         $(this).addClass('active');
     });
-    
+
     /*-------------------
 		Nice Select
     --------------------- */
@@ -212,39 +214,62 @@
     /*------------------
 		Single Product
 	--------------------*/
-	$('.product-thumbs-track .pt').on('click', function(){
-		$('.product-thumbs-track .pt').removeClass('active');
-		$(this).addClass('active');
-		var imgurl = $(this).data('imgbigurl');
-		var bigImg = $('.product-big-img').attr('src');
-		if(imgurl != bigImg) {
-			$('.product-big-img').attr({src: imgurl});
-			$('.zoomImg').attr({src: imgurl});
-		}
-	});
+    $('.product-thumbs-track .pt').on('click', function() {
+        $('.product-thumbs-track .pt').removeClass('active');
+        $(this).addClass('active');
+        let imgurl = $(this).data('imgbigurl');
+        let bigImg = $('.product-big-img').attr('src');
+        if (imgurl != bigImg) {
+            $('.product-big-img').attr({ src: imgurl });
+            $('.zoomImg').attr({ src: imgurl });
+        }
+    });
 
     $('.product-pic-zoom').zoom();
-    
+
     /*-------------------
 		Quantity change
 	--------------------- */
-    var proQty = $('.pro-qty');
-	proQty.prepend('<span class="dec qtybtn">-</span>');
-	proQty.append('<span class="inc qtybtn">+</span>');
-	proQty.on('click', '.qtybtn', function () {
-		var $button = $(this);
-		var oldValue = $button.parent().find('input').val();
-		if ($button.hasClass('inc')) {
-			var newVal = parseFloat(oldValue) + 1;
-		} else {
-			// Don't allow decrementing below zero
-			if (oldValue > 0) {
-				var newVal = parseFloat(oldValue) - 1;
-			} else {
-				newVal = 0;
-			}
-		}
-		$button.parent().find('input').val(newVal);
-	});
+    let proQty = $('.pro-qty');
+    proQty.prepend('<span class="dec qtybtn">-</span>');
+    proQty.append('<span class="inc qtybtn">+</span>');
+    proQty.on('click', '.qtybtn', function() {
+        let $button = $(this);
+        let oldValue = $button.parent().find('input').val();
+        if ($button.hasClass('inc')) {
+            let newVal = parseFloat(oldValue) + 1;
+        } else {
+            // Don't allow decrementing below zero
+            if (oldValue > 0) {
+                let newVal = parseFloat(oldValue) - 1;
+            } else {
+                newVal = 0;
+            }
+        }
+        $button.parent().find('input').val(newVal);
+    });
 
 })(jQuery);
+
+// JavaScript
+
+
+var video = document.getElementById('bg-video');
+var source = document.createElement('source');
+
+source.setAttribute('src', 'img/beone-video.mp4');
+source.setAttribute('type', 'video/mp4');
+
+video.appendChild(source);
+video.play();
+
+setTimeout(function() {
+    video.pause();
+
+    source.setAttribute('src', 'img/vtv3 Beone.mp4');
+    source.setAttribute('type', 'video/mp4');
+
+    video.load();
+    video.play();
+
+}, 700000);
